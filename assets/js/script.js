f899ab1907f9a65710f9a50d807f9483d09f5828
jadehuynh <jadehuynh08@gmail.com>
2022-03-30 17:35:11 -0500

    structure the div containers and attempted to start the functions for color coding

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+$('#root');
+
+var jumbotron = $(".jumbotron");
+var description = $(".description");
+var timeBlock = $(".time-block");
+var rowSection = $(".row");
+var hourDisplay = $(".hour");
+var pastDisplay = $(".past");
+var presentDisplay = $(".present");
+var futureDisplay = $(".future");
+var saveButton = $(".saveBtn")
+
+var currentDate = moment().format("MMMM Do YYYY");
+console.log(currentDate)
+
+var currentDateDisplay = $("#currentDay")
+currentDateDisplay.text(currentDate);
+
+description.each(function() {
+    var rightNow = $(this).parent().parent().attr("id")
+    var currentHour = moment().hour()
+    console.log(rightNow, currentHour)
+    if(parseInt(rightNow)=== currentHour) {
+        $(this).addClass("present")
+    }
+    else if (parseInt(rightNow)> currentHour) {
+        $(this).addClass("future")
+    }
+    else {
+        $(this).addClass("past")
+    }
+})
+
---
+++ pseudocode.md
@@ -0,0 +1,35 @@
+// 05 Third-Party APIs: Work Day Scheduler Psuedocode
+
+- Make sure all the links to CDNs are working properly
+
+- Create basic HTML to resemble mockup
+	- Create 9 rows with 3 columns each
+	- Hour column will be size 1
+	- Description column will be size 10
+	- Savebutton column will be size 1
+
+- Set up an event handler for our save Button
+	- Listen for clicks on each time blocks save button, then save to local storage
+
+- Load any saved data from localStorage
+
+- Figure out how to create our text area for the description
+
+- Display the day, month , and day of month 
+- Moment.js, figure out formatting
+	- How to track hours
+	- startOf() function?
+
+- Color coding of time blocks
+	- If current hour > time block hour set it to past
+	- If current hour === time block hour set it to present
+	- Otherwise set time blocks to future
+
+
+
+
+
+
+
+
+
