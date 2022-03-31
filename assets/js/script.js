ad33cb68a08c044bc130e2a23c78ce0ce775530a
jadehuynh <jadehuynh08@gmail.com>
2022-03-31 08:58:10 -0500

    added if function

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,7 @@ currentDateDisplay.text(currentDate);
 description.each(function() {
     var rightNow = $(this).parent().parent().attr("id")
     var currentHour = moment().hour()
-    console.log(rightNow, currentHour)
+    //console.log(rightNow, currentHour)
     if(parseInt(rightNow)=== currentHour) {
         $(this).addClass("present")
     }
@@ -32,27 +32,31 @@ description.each(function() {
 })
 
 
-var textElement = $("#textSpace")
-var saveItem =localStorage.setItem($("textElement"), 'uff da')
 
-var saveBtn9 = $("#save9")
-var saveBtn10 = $("#save10")
-var saveBtn11 = $("#save11")
-var saveBtn12 = $("#save12")
-var saveBtn13 = $("#save13")
-var saveBtn14 = $("#save14")
-var saveBtn15 = $("#save15")
-var saveBtn16 = $("#save16")
-var saveBtn17= $("save17")
 
-saveButton= $('saveBtn9').on('click', function (e) {
-   localStorage.setItem('feed', 'me')
+// var saveBtn9 = $("#save9")
+// var saveBtn10 = $("#save10")
+// var saveBtn11 = $("#save11")
+// var saveBtn12 = $("#save12")
+// var saveBtn13 = $("#save13")
+// var saveBtn14 = $("#save14")
+// var saveBtn15 = $("#save15")
+// var saveBtn16 = $("#save16")
+// var saveBtn17= $("save17")
+
+var textElement = $(textArea).text("");
+var saveItem =localStorage.setItem('planEntry',textElement)
+
+saveButton= $('saveButton').on('click', function (e) {
     e.preventDefault();
-    saveItem(saveItem);
-    textSpace.text("ryryry");
+    if (saveButton === saveItem) {
+        //
+    localStorage.setItem('enterPlans', JSON.stringify(textElement));
+    }  
+        
 });
 
-console.log(textElement.text);
+console.log(textElement);
 
 
 
