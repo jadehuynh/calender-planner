168b2eb01ee4762f0ef5d6f7e6811fd81c81a29c
jadehuynh <jadehuynh08@gmail.com>
2022-03-30 23:20:42 -0500

    added save button variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,14 +32,28 @@ description.each(function() {
 })
 
 
+var textElement = $("#textSpace")
+var saveItem =localStorage.setItem($("textElement"), 'uff da')
 
-$('saveButton').click(function (e) {
-    var saveItem =localStorage.setItem("W","W");
-    localStorage.getItem(saveItem);
+var saveBtn9 = $("#save9")
+var saveBtn10 = $("#save10")
+var saveBtn11 = $("#save11")
+var saveBtn12 = $("#save12")
+var saveBtn13 = $("#save13")
+var saveBtn14 = $("#save14")
+var saveBtn15 = $("#save15")
+var saveBtn16 = $("#save16")
+var saveBtn17= $("save17")
+
+saveButton= $('saveBtn9').on('click', function (e) {
+   localStorage.setItem('feed', 'me')
     e.preventDefault();
-
-    saveItem();
-    console.log.text(saveItem)
+    saveItem(saveItem);
+    textSpace.text("ryryry");
 });
+
+console.log(textElement.text);
+
+
 
 
